5e41fb907c52f155bba0a0782aab40519c736de7
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2020-09-11 11:28:10 -0300

    primeiro commit

---
+++ src/test/java/com/mateus/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
