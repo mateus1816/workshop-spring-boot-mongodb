e1bd2d7f6c8413ba57c86f20aa9e6a6fed91c226
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2020-09-15 11:20:15 -0300

    Obtendo um usuário por id

--- src/main/java/com/mateus/workshopmongo/services/UserService.java
+++ src/main/java/com/mateus/workshopmongo/services/UserService.java
@@ -1,21 +1,27 @@
 package com.mateus.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mateus.workshopmongo.domain.User;
 import com.mateus.workshopmongo.repository.UserRepository;
+import com.mateus.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
