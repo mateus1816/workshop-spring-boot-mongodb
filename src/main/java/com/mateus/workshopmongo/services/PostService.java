08327478945cfec8eb08071b0bf8711e9aafbd7d
Mateus Rodrigues <mateus.rodrigues05@gmail.com>
2020-09-22 23:51:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mateus/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.mateus.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mateus.workshopmongo.domain.Post;
+import com.mateus.workshopmongo.repository.PostRepository;
+import com.mateus.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	
+}
